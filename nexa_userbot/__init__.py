b3cab552fb67164223f16ef50e9d49e950dcd006
Nikitaroy31 <otpchatappservice@gmail.com>
2022-05-03 14:26:04 +0530

    first commit

---
+++ nexa_userbot/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2021 Itz-fork
+# Part of: Nexa-Userbot
+from time import time
+from pyrogram import Client
+from config import Config
+
+# Configs
+CMD_HELP = {}
+StartTime = time()
+
+NEXAUB = Client(
+    api_hash=Config.API_HASH,
+    api_id=Config.APP_ID,
+    session_name=Config.PYRO_STR_SESSION,
+    sleep_threshold=10
+)
+
+bot = Client(
+    ":nikki:",
+    api_hash=Config.API_HASH,
+    api_id=Config.APP_ID,
+    bot_token=Config.BOT_TOKEN,
+)
